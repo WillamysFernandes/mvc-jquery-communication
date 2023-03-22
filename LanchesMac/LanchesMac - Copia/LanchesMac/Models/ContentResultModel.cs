f3f9ae91cb01e818588353ca22c0cdd8fb4f1e22
Willamys Fernandes <willamysx7@gmail.com>
2023-03-22 01:30:43 -0300

    ok

--- LanchesMac/LanchesMac - Copia/LanchesMac/Models/ContentResultModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LanchesMac.Models
-{
-    public class ContentResultModel
-    {
-        public bool Status { get; set; }
-        public string Message { get; set; }
-    }
-}
--- LanchesMac/LanchesMac - Copia/LanchesMac/ViewModels/EditViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using LanchesMac.Models;
-
-namespace LanchesMac.ViewModels
-{
-	public class EditViewModel
-	{
-		public User User  { get; set; }
-	}
-}
